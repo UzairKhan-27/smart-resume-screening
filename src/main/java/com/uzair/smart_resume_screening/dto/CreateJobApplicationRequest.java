af8ba71e9aa2e6cad7ad2f6cf4109352a8311383
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2025-08-05 15:53:15 +0500

    add not empty validation

--- src/main/java/com/uzair/smart_resume_screening/dto/CreateJobApplicationRequest.java
+++ src/main/java/com/uzair/smart_resume_screening/dto/CreateJobApplicationRequest.java
@@ -1,7 +1,8 @@
 package com.uzair.smart_resume_screening.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-public record CreateJobApplicationRequest(@NotNull @Min(1) int job_id, @NotNull @Min(1) int person_id) {
+public record CreateJobApplicationRequest(@NotNull @Min(1) int job_id, @NotEmpty String email) {
 }
